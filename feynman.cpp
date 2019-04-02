ba58d46d2ce5d97aafc4a1791f954ee7e4c63a0d
andyy143 <35781193+andyy143@users.noreply.github.com>
2019-04-02 22:32:37 +0530

    Add files via upload

---
+++ feynman.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+const ll N=10000;
+using namespace std;
+
+int main()
+{
+	ll t,n;
+	while(1)
+	{
+		cin>>n;
+		if(n==0)
+		return 0;
+		cout<<(n*(n+1)*(2*n+1))/6<<endl;
+	}
+														
+}
